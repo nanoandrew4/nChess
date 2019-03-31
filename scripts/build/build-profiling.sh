cd02a7a03373775fef4c019f1c1f405741a902fd
nanoandrew4 <nanoandrew4@gmail.com>
2019-03-31 17:56:59 +0200

    Added scripts to build different targets using profile guided optimization. Curiously, the binaries generated by the PGO scripts run noticeably slower than non-PGO binaries, so for now regular binaries should be used.

--- scripts/build/build-profiling.sh
+++ scripts/build/build-profiling.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Generates a Release executable using profile guided optimization
+# Generates a Profiling executable using profile guided optimization
 # Requires perf, cmake v3.13.x, clang, lld, autofdo
 
-cd ../../cmake-build-release
+cd ../../cmake-build-profiling
 echo "Starting initial compilation"
 
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_PGO=Off -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "CodeBlocks - Unix Makefiles" ../
+cmake -DCMAKE_BUILD_TYPE=Profiling -DENABLE_PGO=Off -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "CodeBlocks - Unix Makefiles" ../
 cmake --build . --target clean -- -j 4
 cmake --build . --target all -- -j 4
 
@@ -17,10 +17,10 @@ sudo chmod 777 perf.data
 
 printf "\n\n"
 echo "Creating LLVM profile data for use in next compilation pass"
-create_llvm_prof --binary=./nChess --out=nChess-rel.prof
+create_llvm_prof --binary=./nChess --out=nChess-prof.prof
 
 echo "Compiling using Profile Guided Optimization, based on the recently produced profile"
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_PGO=On -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "CodeBlocks - Unix Makefiles" ../
+cmake -DCMAKE_BUILD_TYPE=Profiling -DENABLE_PGO=On -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "CodeBlocks - Unix Makefiles" ../
 cmake --build . --target clean -- -j 4
 cmake --build . --target all -- -j 4
 
